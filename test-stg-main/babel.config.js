737e40e8fce3ded6130d90e4dc99e69aef4ff548
atakangngr <64753495+atakangngr@users.noreply.github.com>
2022-09-19 15:21:08 +0300

    Delete test-stg-main directory

--- test-stg-main/babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-nullish-coalescing-operator'],
-  presets: [process.env.VUE_PLATFORM === 'web' ? '@vue/cli-plugin-babel/preset' : {}, ['@babel/env', { targets: { esmodules: true } }]],
-};
